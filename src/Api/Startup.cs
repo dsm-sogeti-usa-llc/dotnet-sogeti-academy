b4592f32ce271368199149e9d4332aed8ffb6405
Bryce Klinker <bryce.klinker@us.sogeti.com>
2016-04-15 00:35:56 -0500

    Properly configuring telemetry for API and MVC applications

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,15 +1,31 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Owin;
 using Sogeti.Academy.Api;
+using Sogeti.Academy.Infrastructure.Configuration;
 
 [assembly:OwinStartup(typeof(Startup))]
 namespace Sogeti.Academy.Api
 {
     public class Startup
     {
+        private readonly string _instrumentationKey;
+
+        public Startup()
+            : this(new Configuration())
+        {
+            
+        }
+
+        public Startup(IConfiguration configuration)
+        {
+            _instrumentationKey = configuration["ApplicationInsights:InstrumentationKey"];
+        }
+
         public void Configuration(IAppBuilder app)
         {
+            TelemetryConfiguration.Active.InstrumentationKey = _instrumentationKey;
             app.UseErrorPage();
             app.UseCors(CorsOptions.AllowAll);
             app.UseWebApi(WebApiConfiguration.Create());
--- src/Mvc/Global.asax.cs
+++ src/Mvc/Global.asax.cs
@@ -2,15 +2,30 @@
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Microsoft.ApplicationInsights.Extensibility;
+using Sogeti.Academy.Infrastructure.Configuration;
 using Sogeti.Academy.Mvc.General;
 
 namespace Sogeti.Academy.Mvc
 {
     public class Global : HttpApplication
     {
+        private readonly string _instrumentationKey;
+
+        public Global()
+            : this(new Configuration())
+        {
+            
+        }
+
+        public Global(IConfiguration configuration)
+        {
+            _instrumentationKey = configuration["ApplicationInsights:InstrumentationKey"];
+        }
 
         protected void Application_Start(object sender, EventArgs e)
         {
+            TelemetryConfiguration.Active.InstrumentationKey = _instrumentationKey;
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new FeatureViewEngine());
             RouteConfig.RegisterRoutes(RouteTable.Routes);
